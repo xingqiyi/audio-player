9ac499569d3098ee2179b55e908a9522eea3de74
shuaixiaocheng <448378892@qq.com>
2019-08-22 16:05:56 +0800

    audio

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MyComponent from '../../src';
+
+const amr_url = 'http://39.106.123.19:8900/static/086ed7ad99059df557d117bdf22851f2-1566292439418.amr';
+const mp3_url = 'http://39.106.123.19:8900/static/7e59567e3c8635bd68b5d512f7c9c1d7-1566219400810.mp3';
+
+
 const App = () => (
-    <MyComponent />
+    <div>
+        <div>
+            <h3>amr:</h3>
+            <MyComponent src={amr_url} />
+        </div>
+
+        <div>
+            <h3>mp3:</h3>
+            <MyComponent src={mp3_url} />
+        </div>
+
+    </div>
+
 );
 render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import BenzAMRRecorder from 'benz-amr-recorder'
+
 import './styles.css'
 
-const MyComponent = () => (
-    <h1>Hello from my component.</h1>
-);
+const playHandler = (amr, player) => {
+    player.then(function () {
+        amr.play();
+    });
+}
+const stopHandler = (amr) => {
+    amr.stop();
+}
+
+const MyComponent = ({ src }) => {
+
+
+    const fileExtension = src.split('.').pop().toLowerCase();
+
+    if (fileExtension === 'amr') {
+        const amr = new BenzAMRRecorder();
+        const player = amr.initWithUrl(src);
+        amr.onEnded(function () {
+            console.log('播放完毕');
+        })
+
+        return (
+            <div>
+                <h1>ello from my component.</h1>
+                <h4>{src}</h4>
+                <input type='button' onClick={playHandler.bind(this, amr, player)} value='播放'></input>
+                <input type='button' onClick={playHandler.bind(this, amr)} value='停止'></input>
+            </div>
+
+
+        )
+    } else if (['mp3', 'wav', 'ogg'].includes(fileExtension)) {
+        return (
+            <audio src={src} controls="controls"> </audio>
+
+        )
+    }
+};
 
 export default MyComponent;
