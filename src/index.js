96bae6b75e084e71a3a9bf31f53ba492e0453eb2
shuaixiaocheng <448378892@qq.com>
2019-08-22 14:56:45 +0800

    .

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css'
 
 const MyComponent = () => (
-    <h1>Hello from my component</h1>
+    <h1>Hello from my component.</h1>
 );
 
 export default MyComponent;
